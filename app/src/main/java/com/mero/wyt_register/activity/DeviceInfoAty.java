c9078237dfe01f8aede22085571aa09b33ab820d
chenlei <790710371@qq.com>
2016-10-16 14:09:35 +0800

    update the deviceInfoAty.java and the xml

--- app/src/main/java/com/mero/wyt_register/activity/DeviceInfoAty.java
+++ app/src/main/java/com/mero/wyt_register/activity/DeviceInfoAty.java
@@ -1,17 +1,39 @@
 package com.mero.wyt_register.activity;
 
 import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.mero.wyt_register.MainActivity;
 import com.mero.wyt_register.R;
+import com.mero.wyt_register.utils.DeviceUtils;
 import com.mero.wyt_register.widget.CustomTitleBar;
 
+import static android.content.ContentValues.TAG;
+import static com.mero.wyt_register.R.id.edt_IMEI;
+import static com.mero.wyt_register.R.id.edt_phone_country;
+import static com.mero.wyt_register.R.id.edt_sim_xulie_num;
+
 public class DeviceInfoAty extends Activity {
+    private static final String TAG = "DeviceInfoAty";
     private CustomTitleBar customTitleBar;
+    private EditText edt_sim_xulie_num;//序列号
+    private EditText edt_IMEI;//IMEI
+    private EditText edt_IMSI;//IMSI
+    private EditText edt_phone;//手机号码
+    private EditText edt_phone_xulie_num;//手机卡序列号
+    private EditText edt_phone_country;//手机卡国家
+    private EditText edt_yunyingshang_code;//运营商代码
+    private EditText edt_yunyingshang;//运营商
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +45,13 @@ public class DeviceInfoAty extends Activity {
         setContentView(R.layout.main_device_info);
         initView();
     }
-
+    private String simNum;//Sim卡序列号
+    private String IMEI;//IMEI
+    private String IMSI;//IMSI
+    private String phoneNum;//手机号码
+    private String countryCode;//国家代码
+    private String providerCode;//运营商代码
+    private String providerName;//运营商名字
     private void initView() {
         customTitleBar = (CustomTitleBar) findViewById(R.id.title_bar_device_info);
         customTitleBar.onClick(new CustomTitleBar.TitleBarImageListener() {
@@ -32,5 +60,28 @@ public class DeviceInfoAty extends Activity {
                 startActivity(new Intent(DeviceInfoAty.this, MainActivity.class));
             }
         });
+
+        //初始化手机数据
+        edt_IMEI = (EditText) findViewById(R.id.edt_IMEI);
+        edt_IMSI = (EditText) findViewById(R.id.edt_IMSI);
+        edt_phone = (EditText) findViewById(R.id.edt_phone);
+        edt_phone_country = (EditText) findViewById(R.id.edt_phone_country);
+        edt_sim_xulie_num = (EditText) findViewById(R.id.edt_sim_xulie_num);
+        edt_yunyingshang = (EditText) findViewById(R.id.edt_yunyingshang);
+        edt_yunyingshang_code = (EditText) findViewById(R.id.edt_yuunyingshang_code);
+        simNum = DeviceUtils.getSimNumber(this);//获取手机卡序列号
+        IMEI = DeviceUtils.getIMEI(this);//获取IMEI
+        IMSI = DeviceUtils.getIMSI(this);//获取IMSI
+        phoneNum = DeviceUtils.getPhoneNum(this);//获取手机号
+        countryCode = DeviceUtils.getCountryZipCode(this);//获取国家代码
+        providerCode = DeviceUtils.getSubScriberId(this);//获取运营商代码
+        providerName = DeviceUtils.getProviderInfo(this);
+        edt_sim_xulie_num.setText(simNum);//设置手机卡序列号
+        edt_IMEI.setText(IMEI);//设置手机IMEI
+        edt_IMSI.setText(IMSI);//设置手机IMSI
+        edt_phone.setText(phoneNum);//设置手机号
+        edt_phone_country.setText(countryCode);//设置国家代码
+        edt_yunyingshang_code.setText(providerCode);//设置运营商代码
+        edt_yunyingshang.setText(providerName);//设置运营商
     }
 }
--- app/src/main/java/com/mero/wyt_register/utils/DeviceUtils.java
+++ app/src/main/java/com/mero/wyt_register/utils/DeviceUtils.java
@@ -2,26 +2,123 @@ package com.mero.wyt_register.utils;
 
 import android.content.Context;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.mero.wyt_register.R;
 
 import java.util.concurrent.ExecutionException;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by chenlei on 2016/10/15.
  */
 
 public class DeviceUtils {
+    private static  final String TAG = "DeviceUtils";
     private DeviceUtils(){
         throw new ExceptionInInitializerError("不可实例化");
     }
-    public static String getDeviceId(Context context){
-        String deviceId = "";
+    public static String getIMEI(Context context){
+        String IMEI= "";
         TelephonyManager tm = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
         try{
-           deviceId = tm.getDeviceId();
+            IMEI = tm.getDeviceId();
+            if(TextUtils.isEmpty(tm.getDeviceId())){
+                IMEI = "IMEI无效";
+            }
         }catch (Exception e){
-          deviceId = "获取deviceId失败";
+          String msg = e.getMessage()==null?"e.getMessage为空":e.getMessage();
+            Log.e(TAG,msg);
+          IMEI = "获取deviceId失败";
+        }
+        return IMEI;
+    }
+    //获取手机号
+    public static String getPhoneNum(Context context){
+        String phoneNum = "";
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        try{
+            phoneNum = tm.getLine1Number();
+        }catch (Exception e){
+            phoneNum = "获取手机号失败"+e.getMessage();
         }finally {
-            return deviceId;
+            return phoneNum;
+        }
+    }
+    //获取手机国家
+    public static String getCountryZipCode(Context context) {
+        String countryID = "";
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        try {
+            countryID = tm.getSimCountryIso();
+        }catch (Exception e){
+            countryID = "获取国家码失败";
+        }finally {
+            return countryID;
+        }
+    }
+    //获取手机运营商信息
+    public static String getProviderInfo(Context context){
+        String providerInfo= "";
+        String IMSI="";
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        try{
+            IMSI = tm.getSubscriberId();
+            if(IMSI.startsWith("46000")||IMSI.startsWith("46002")){
+                providerInfo = "中国移动";
+            }
+            if(IMSI.startsWith("46001")){
+                providerInfo = "中国联通";
+            }
+            if(IMSI.startsWith("46003")){
+                providerInfo = "中国电信";
+            }
+        }catch (Exception e){
+            providerInfo = "获取运营商信息失败"+e.getMessage();
+        }finally {
+            return providerInfo;
+        }
+    }
+    //获取运营商代码
+    public static String getSubScriberId(Context context){
+        String subScriberId= "";
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        try{
+            subScriberId = tm.getSubscriberId();
+        }catch (Exception e){
+            Log.e(TAG,e.getMessage()==null?"e.getMessage()为null":e.getMessage());
+            subScriberId = "获取运营商代码失败";
+        }finally {
+            return subScriberId;
+        }
+    }
+    //获取IMSI
+    public static String getIMSI(Context context){
+        String IMSI = "";
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        try{
+            IMSI = tm.getSubscriberId();
+        }catch (Exception e){
+            IMSI = "获取IMSI失败"+e.getMessage();
+        }finally {
+            return  IMSI;
+        }
+    }
+
+    //获取sim卡序列号
+    public static String getSimNumber(Context context){
+        String simNumber = "";
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        try{
+            simNumber = tm.getSimSerialNumber();
+        }catch (Exception e){
+            String msg = e.getMessage()==null?"e.getMessage()为空":e.getMessage();
+            Log.e(TAG,msg);
+            simNumber = "获取sim卡序列号失败";
+        }finally {
+            return simNumber;
         }
     }
 }
