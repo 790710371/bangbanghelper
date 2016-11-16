a9d28c26944023bd0a39f5b61d3c63e284b4b642
chenlei <790710371@qq.com>
2016-11-16 12:23:26 +0800

    更新config配置

--- app/src/main/java/com/mero/wyt_register/Config.java
+++ app/src/main/java/com/mero/wyt_register/Config.java
@@ -1,4 +1,8 @@
 package com.mero.wyt_register;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  *@项目名称: 简易通注册助手
  *@文件名称: Config.java
@@ -14,8 +18,25 @@ public class Config {
 	public static final String KEY_IS_FIRST_IN = "isFirstIn";
 	public static final String KEY_IS_INSTALL_XPOSED = "isInstalledXposed";//是否安装XPOSED
 	public static final String KEY_IS_MODULE_INSTALLED = "isInstalledModule";//是否已经安装MODULE
+	public static final String KEY_STATUS = "status";//状态key
+	public static final String KEY_TOKEN = "token";
 	public static final boolean VALUE_TRUE = true;
 	public static final boolean VALUE_FALSE = false;
 	public static final String VALUE_IS_INSTALL = "已安装";
 	public static final String VALUE_NOT_INSTALLED = "未安装";
+
+
+	public void setTokenPreferences(Context context,String token){
+		SharedPreferences sharedPreferences = context.getSharedPreferences(Config.ID,Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = 	sharedPreferences.edit();
+		editor.putString(KEY_TOKEN,token);
+		editor.commit();
+	}
+
+	public String getTokenFromPreferences(Context context){
+		String token = "";
+		SharedPreferences sharedPreferences  = context.getSharedPreferences(Config.ID,Context.MODE_PRIVATE);
+		token = sharedPreferences.getString(KEY_TOKEN,null);
+		return token;
+	}
 }
--- app/src/main/java/com/mero/wyt_register/fragment/MenuFragment.java
+++ app/src/main/java/com/mero/wyt_register/fragment/MenuFragment.java
@@ -1,24 +1,45 @@
 package com.mero.wyt_register.fragment;
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.mero.wyt_register.Config;
 import com.mero.wyt_register.R;
 
 /**
  * Created by chenlei on 2016/11/13.
  */
 
-public class MenuFragment extends Fragment{
+public class MenuFragment extends Fragment implements View.OnClickListener{
     public static final String TAG  = "MenuFragment";
+    private View view;
+    private ImageView img_menu_login;//登录
+    private TextView tx_user_name;//用户名
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.menu,container,false);
-        ImageView imageView01 = (ImageView) view.findViewById(R.id.img_memu_item_01);
+        view = inflater.inflate(R.layout.menu,container,false);
+        initView();
         return view;
+    }
+
+    private void initView() {
+        img_menu_login = (ImageView) view.findViewById(R.id.img_menu_icon);
+        tx_user_name = (TextView) view.findViewById(R.id.tx_menu_user_name);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.img_menu_icon:
+                //点击头像登录
+            
+                break;
+        }
     }
 }
